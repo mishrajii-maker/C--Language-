6ce91a184c5093ebb1305f3b84f53c5b057df150
mishrajii-maker <mishrajiiofficial05@gmail.com>
2026-03-03 17:48:20 +0530

    String1.c

---
+++ String1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+    char str1[50] = "Hello Good Morning", str2[10];
+    // strcpy(str2, str1);
+    // strcpy(str3, strcpy(str2, str1));
+    // printf("%s\n", str2);
+    // printf("%s\n", str3);
+
+    // strncpy(str2, str1, sizeof(str2));
+    // str2[sizeof(str2) - 1] = '\0';
+    // printf("%s", str2);
+
+    return 0;
+}
