66aa9b21b42fc8c3316d1d5e7e30ff9956da0496
mishrajii-maker <mishrajiiofficial05@gmail.com>
2026-03-03 18:17:17 +0530

    Loop.c

---
+++ Loop.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+    /*
+        For Loop
+        While Loop
+        Do While Loop
+    */
+
+    int i = 1;
+
+    do{
+        printf("Hello World %d\n", i);
+        i++; // 11
+    }while(i <= 10);
+
+    return 0;
+}
