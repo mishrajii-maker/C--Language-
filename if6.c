59b6cdf7237dc9dbb0790b5b144ac2336f775c3e
mishrajii-maker <mishrajiiofficial05@gmail.com>
2026-03-03 18:11:36 +0530

    if6.c

---
+++ if6.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+
+    char ch;
+    printf("Enter any Character: ");
+    scanf("%c", &ch);
+
+    if (ch >= 'A' && ch <= 'Z')
+    {
+        printf("Uppercase Letter\n");
+    }
+    else if (ch >= 'a' && ch <= 'z')
+    {
+        printf("Lowercase Letter\n");
+    }
+    else if (ch >= '0' && ch <= '9')
+    {
+        printf("Digit\n");
+    }
+    else
+    {
+        printf("Special Character\n");
+    }
+    
+    
+
+    return 0;
+}
